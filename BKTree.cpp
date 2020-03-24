2c518ee3a102658acf78007b9d51c230ba4ffc61
Aditya Singh <singh.aditya8499@gmail.com>
2020-03-24 14:10:22 +0530

    Created EditDistance function and add node function

--- BKTree.cpp
+++ BKTree.cpp
@@ -21,15 +21,56 @@ struct Node   //stored information about our node
     Node()
     {
         // default constructor
+        word="";
+        for(int i=0;i<LEN;i++)
+        {
+            next[i]=0;
+        }
     }
 };
 Node root;   //root node of our tree
 vector<Node> nodes;  //stores all the nodes
 int node_count=0;   
 
+int findEditDistance(string a,string b)
+{
+    int n = a.length();
+    int m = b.length();
+    int arr[n+1][m+1];
+    memset(arr,0,sizeof arr);
+    for(int i=0;i<=m;i++)
+    {
+        arr[0][i]=i;
+    }
+    for(int i=0;j<=n;i++)
+    {
+        arr[i][0]=i;
+    }
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=m;j++)
+        {
+            if(a[i-1]!=b[j-1])
+            {
+                arr[i][j]=1+min(arr[i-1][j],min(arr[i-1][j-1],arr[i][j-1]));
+            }
+            else
+            {
+                arr[i][j]=arr[i-1][j-1];
+            }
+        }
+    }
+    return arr[n][m];
+}
+
 void add(Node &root,Node tmp)
 {
-    
+    if(root.word=="")
+    {
+        root=tmp;
+        return;
+    }
+    int editDistance = findEditDistance(root.word,tmp.word);
 }
 int main()
 {
