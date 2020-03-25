89752c74264e63af98d01fe67910093fa5dff825
Aditya Singh <singh.aditya8499@gmail.com>
2020-03-25 10:39:33 +0530

    Completed the full code

--- BKTree.cpp
+++ BKTree.cpp
@@ -10,10 +10,10 @@ struct Node   //stored information about our node
 {
     string word;
     int next[LEN];
-    Node(string s)   //parameterized constructor
+    Node(string s):word(s)   //parameterized constructor
     {
         word=s;
-        for(int i=0;i<LEN;i++)
+        for(int i=0;i<LEN;i++)  //initialize all the data items
         {
             next[i]=0;
         }
@@ -21,18 +21,13 @@ struct Node   //stored information about our node
     Node()
     {
         // default constructor
-        word="";
-        for(int i=0;i<LEN;i++)
-        {
-            next[i]=0;
-        }
     }
 };
 Node root;   //root node of our tree
-vector<Node> nodes;  //stores all the nodes
+Node nodes[100];  //stores all the nodes
 int node_count=0;   
 
-int findEditDistance(string a,string b)
+int findEditDistance(string a,string b)   //find edit distance between strings i.e. Levenshtein distance
 {
     int n = a.length();
     int m = b.length();
@@ -42,7 +37,7 @@ int findEditDistance(string a,string b)
     {
         arr[0][i]=i;
     }
-    for(int i=0;j<=n;i++)
+    for(int i=0;i<=n;i++)
     {
         arr[i][0]=i;
     }
@@ -63,7 +58,7 @@ int findEditDistance(string a,string b)
     return arr[n][m];
 }
 
-void add(Node &root,Node tmp)
+void add(Node &root,Node &tmp)      //add nodes to the tree
 {
     if(root.word=="")
     {
@@ -71,6 +66,46 @@ void add(Node &root,Node tmp)
         return;
     }
     int editDistance = findEditDistance(root.word,tmp.word);
+
+    if(nodes[root.next[editDistance]].word=="")
+    {
+        node_count++;
+        nodes[node_count]=(tmp);
+        root.next[editDistance]=node_count;
+    }
+    else
+    {
+        add(nodes[root.next[editDistance]],tmp);
+    }
+    
+}
+vector<string> getWords(Node &root,string s)           //find the approximate matching strings. Use trainagular inequality here
+{
+    vector<string> matched;
+    if(root.word=="")
+    {
+        return matched;
+    }
+    int distance = findEditDistance(root.word,s);
+    if(distance<=THRS)
+    {
+        matched.push_back(root.word);
+    }
+    int start = distance - THRS;
+    if(start<0)
+    {
+        start=1;
+    }
+    while(start<distance+THRS)
+    {
+        vector<string> tmp = getWords(nodes[root.next[start]],s);
+        for(int i=0;i<tmp.size();i++)
+        {
+            matched.push_back(tmp[i]);
+        }
+        start++;
+    }
+    return matched;
 }
 int main()
 {
@@ -80,4 +115,13 @@ int main()
         Node tmp=Node(dict[i]);
         add(root,tmp);
     }
+
+    string query = "hold";      //query string
+    vector<string> matched = getWords(root,query);      //store all the words here
+    cout<<"Matched words for the query "<<query<<" are: \n";
+    for(int i=0;i<matched.size();i++)
+    {
+        cout<<matched[i]<<"\n";         //output all the resultant strings of the query
+    }
+    return 0;
 }
